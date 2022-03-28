88b8e810d37e6a2a4415a4553e7dd685bb06a555
dks2922 <dks2922@knou.ac.kr>
2022-03-28 23:18:56 +0900

    feat : Entity 클래스 생성

---
+++ src/main/java/jpabook/jpashop/domain/DeliveryStatus.java
@@ -0,0 +1,16 @@
+package jpabook.jpashop.domain;
+
+//import javax.persistence.Entity;
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.Id;
+//import javax.persistence.Table;
+
+import javax.persistence.GeneratedValue;
+
+//@Entity
+//@Table(name = "delivery_status")
+public class DeliveryStatus {
+
+//    @Id @GeneratedValue
+//    private String id;
+}
--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -3,12 +3,14 @@ package jpabook.jpashop.domain.item;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
+//JOINED : 정교화 된 테이블
+//SINGLE_TABLE : 한 테이블에 다 넣는 것
+//TABLE_PER_CLASS : 3개의 테이블만 나오는 전략
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "dtype")
 @Getter @Setter
 public abstract class Item {
 
@@ -20,4 +22,6 @@ public abstract class Item {
     private String name;
     private int price;
     private int stockQuantity;
+
 }
+
