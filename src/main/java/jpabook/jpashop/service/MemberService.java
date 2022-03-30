8b783023ac67705c636b09210f6f98afde955e9e
dks2922 <dks2922@knou.ac.kr>
2022-03-30 23:32:28 +0900

    feat : 회원 service 추가

---
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -0,0 +1,21 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MemberService {
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    //회원가입
+
+    //회원 전체 조회
+
+
+
+
+}
