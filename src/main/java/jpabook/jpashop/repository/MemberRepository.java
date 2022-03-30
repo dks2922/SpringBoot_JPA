e4160d3444b1ca5d91bd7c89c7585fd500d9b301
dks2922 <dks2922@knou.ac.kr>
2022-03-30 23:55:33 +0900

    feat : 생성자 주입 어노테이션 추가

--- src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.repository;
 
 import jpabook.jpashop.domain.Member;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
@@ -8,11 +9,11 @@ import javax.persistence.PersistenceContext;
 import java.util.List;
 
 @Repository
+@RequiredArgsConstructor
 public class MemberRepository {
 
     //spring이 Entity Manager를 만들어서 걔를 em이란 변수명에 주입(injection)한다.
-    @PersistenceContext
-    private EntityManager em;
+    private final EntityManager em;
 
 
     public void save (Member member){
