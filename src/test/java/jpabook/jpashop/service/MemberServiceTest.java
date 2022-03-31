1f1855ced2668b5f890475890832e666e21ea7ef
kosmos <dks2922@gmail.com>
2022-03-31 15:10:38 +0900

    feat : 단위테스트 = 회원가입 예외처리 추가

--- src/test/java/jpabook/jpashop/service/MemberServiceTest.java
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -10,7 +10,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
 
 import static org.junit.Assert.*;
 
@@ -21,23 +24,44 @@ public class MemberServiceTest {
 
     @Autowired MemberService memberService;
     @Autowired MemberRepository memberRepository;
-
+    @Autowired EntityManager em;
 
     @Test
     @Rollback(false)
     public void 회원가입() throws Exception {
         //given
         Member member = new Member();
-        member.setName("An");
+        String name = "An";
+        member.setName(name);
 
-        Address address = new Address("city","1702","18147");
+        String city = "city";
+        String street = "street";
+        String zipcode = "zipcode";
+
+
+        Address address = new Address(city,street,zipcode);
         member.setAddress(address);
 
         //when
         Long saveId = memberService.join(member);
 
         //then
+        em.flush();
         assertEquals(member, memberRepository.findOne(saveId));
     }
 
+    @Test(expected = IllegalStateException.class)
+    public void 회원가입_예외() throws Exception{
+        Member member1 = new Member();
+        member1.setName("kim");
+
+        Member member2 = new Member();
+        member2.setName("kim");
+
+        memberService.join(member1);
+        memberService.join(member2);
+
+
+        Assert.fail("예외 발생 : ");
+    }
 }
