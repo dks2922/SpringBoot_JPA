3ce3967af4be3afabc5a961d2c63da14821a1d58
dks2922 <dks2922@knou.ac.kr>
2022-04-04 23:59:41 +0900

    feat : 주문 기능 테스트 추가

---
+++ src/test/java/jpabook/jpashop/service/OrderServiceTest.java
@@ -0,0 +1,118 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderStatus;
+import jpabook.jpashop.domain.item.Book;
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.exception.NotEnoughtStockException;
+import jpabook.jpashop.repository.OrderRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+public class OrderServiceTest {
+
+    @Autowired EntityManager em;
+    @Autowired OrderService orderService;
+    @Autowired OrderRepository orderRepository;
+
+    @Test
+    public void 상품주문() throws Exception {
+        //given
+        Member member = createMember();
+
+        Book book = createBook("시골JPA",10000,10);
+
+        int orderCount = 2;
+
+        //when
+        Long orderId = orderService.order(member.getId(), book.getId(), orderCount);
+
+        //then
+        Order getOrder = orderRepository.findOne(orderId);
+
+        System.out.println("getStatus : " + getOrder.getStatus());
+        System.out.println("size : " + getOrder.getOrderItems().size());
+        System.out.println("getTotalPrice : " + getOrder.getTotalPrice());
+//        System.out.println("getStockQuantity : " + book.getStockQuantity());
+
+        assertEquals("상품 주문시 상태는 ORDER", OrderStatus.ORDER, getOrder.getStatus());
+        assertEquals("주문한 상품 종류 수가 정확해야 한다.", 1, getOrder.getOrderItems().size());
+        assertEquals("주문 가격은 가격 * 수량이다.", 10000 * orderCount, getOrder.getTotalPrice());
+//        assertEquals("주문 수량만큼 재고가 줄어야 한다.", 12, book.getStockQuantity());
+
+    }
+
+
+    @Test
+    public void 주문취소() throws Exception {
+        //given
+        Member member = createMember();
+        Book item = createBook("시골 JPA", 10000, 10);
+
+        int orderCount = 2;
+        Long orderId = orderService.order(member.getId(), item.getId(), orderCount);
+
+        //when
+        orderService.cancelOrder(orderId);
+
+        //then
+        Order getOrder = orderRepository.findOne(orderId);
+        System.out.println("getOrdergetOrder : " + getOrder);
+
+        assertEquals("주문 취소시 상태는 CANCEL 이다.", OrderStatus.CANCLE, getOrder.getStatus());
+        assertEquals("주문이 취소된 상품은 그만큼 재고가 증가해야 한다.", 10, item.getStockQuantity());
+
+
+   }
+
+    @Test(expected = NotEnoughtStockException.class)
+    public void 상품주문_재고수량초과() throws Exception {
+        //given
+        Member member = createMember();
+        Item item = createBook("시골 JPA",10000, 10);
+
+        System.out.println("item : " + item);
+        int orderCount = 11;
+
+        //when
+        //비고 : orderCount이 stockQuantity 보다 작을 경우 예외 처리가 발생
+        // orderService -> order -> orderItem을 만들어 -> createOrderItem(removeStock) 호출할때 체크하는 로직이 Entity에 들어가 있어 예외 처리가 발생
+        orderService.order(member.getId(), item.getId(), orderCount);
+        System.out.println("orderService : " + orderService);
+
+        //then
+        fail("재고 수량 부족 예외가 발행해야 한다.");
+    }
+
+
+    private Book createBook(String name, int price, int stockQuantity) {
+        Book book = new Book();
+        book.setName(name);
+        book.setPrice(price);
+        book.setStockQuantity(stockQuantity);
+        em.persist(book);
+        return book;
+    }
+
+    private Member createMember() {
+        Member member = new Member();
+        member.setName("회원1");
+        member.setAddress(new Address("서울","강가","123-123"));
+        em.persist(member);
+        return member;
+    }
+
+}
