628239326b02b1dd7feacdb49e8716def4fb540e
dks2922 <dks2922@knou.ac.kr>
2022-04-11 06:50:34 +0900

    feat : 상품 목록(BoookForm) 추가

---
+++ src/main/java/jpabook/jpashop/controller/BookForm.java
@@ -0,0 +1,13 @@
+package jpabook.jpashop.controller;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class BookForm {
+
+    private String author;
+    private String isbn;
+
+}
