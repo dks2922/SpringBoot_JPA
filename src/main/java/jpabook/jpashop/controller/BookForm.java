76920be8a2ea3805c89860660667beab89fcdfd6
dks2922 <dks2922@knou.ac.kr>
2022-04-11 23:53:07 +0900

    feat : 상품 목록 화면 및 ItemController, BoookForm 기능 추가

--- src/main/java/jpabook/jpashop/controller/BookForm.java
+++ src/main/java/jpabook/jpashop/controller/BookForm.java
@@ -7,6 +7,12 @@ import lombok.Setter;
 @Getter @Setter
 public class BookForm {
 
+    private Long id;
+
+    private String name;
+    private int price;
+    private int stockQuantity;
+
     private String author;
     private String isbn;
 
---
+++ src/main/java/jpabook/jpashop/controller/ItmeController.java
@@ -0,0 +1,41 @@
+package jpabook.jpashop.controller;
+
+import jpabook.jpashop.domain.item.Book;
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.service.ItemService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+
+@Controller
+@RequiredArgsConstructor
+public class ItmeController {
+
+    private final ItemService itemService;
+
+    @GetMapping("/items/new")
+    public String createForm(Model model){
+        model.addAttribute("form", new BookForm());
+        return "items/createItemForm";
+    }
+
+    @PostMapping("/items/new")
+    public String create(@Valid BookForm form, BindingResult result){
+        System.out.println("form : " + form);
+        Book book = new Book();
+        book.setStockQuantity(form.getStockQuantity());
+        book.setPrice(form.getPrice());
+        book.setName(form.getName());
+        book.setIsbn(form.getIsbn());
+        book.setAuthor(form.getAuthor());
+
+        itemService.saveItem(book);
+        return "redirect:/";
+    }
+
+}
